e61f16c3faeebe689f54d42f62d961043065bf36
Hervé Beraud <hberaud@redhat.com>
2020-06-09 12:17:46 +0200

    Use unittest.mock instead of mock
    The mock third party library was needed for mock support in py2
    runtimes. Since we now only support py36 and later, we can use the
    standard lib unittest.mock module instead.
    
    Change-Id: If7d7ed6f3b398b48ace903f8307adc64264b2f1b

--- tests/plugins/modules/test_tripleo_shell_script.py
+++ tests/plugins/modules/test_tripleo_shell_script.py
@@ -13,7 +13,7 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-import mock
+from unittest import mock
 
 from plugins.modules import tripleo_shell_script
 from tests import base as tests_base
